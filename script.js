66f8a265fd345faad6fbeedecd621ea3cd72d50a
joseftvvv <josefpepi95@gmail.com>
2023-03-28 10:34:05 +0200

    new files

---
+++ script.js
@@ -0,0 +1,6 @@
+const meinKnopf = document.getElementById("mein-knopf");
+const textAnzeige = document.getElementById("text-anzeige");
+
+meinKnopf.addEventListener("click", function() {
+    textAnzeige.style.display = "block";
+});
